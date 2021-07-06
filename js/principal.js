ced1cd8c30c8bdaf817a6245a16a87d73a5d77e0
Cristian Martini <cristiamcarlosm@hotmail.com>
2021-07-06 17:46:23 -0300

    Selecionando peso do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,9 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#paciente-1");
+var tdPeso = paciente.querySelector(".info-peso");
+
+console.log (paciente);
+console.log (tdPeso);
