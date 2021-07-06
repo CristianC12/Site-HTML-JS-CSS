423d6c8969bc9efb7b4c64ed8964a7123f35396f
Cristian Martini <cristiamcarlosm@hotmail.com>
2021-07-06 18:05:08 -0300

    Adicionando calculo de IMC

--- js/principal.js
+++ js/principal.js
@@ -7,5 +7,7 @@ var paciente = document.querySelector("#paciente-1");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector("-info-altura");
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
