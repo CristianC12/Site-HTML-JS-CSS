e6e35e770fe165cdc3170e14c2ab371d10261f21
Cristian Martini <cristiamcarlosm@hotmail.com>
2021-07-06 18:16:01 -0300

    resultado do IMC no HTML

--- js/principal.js
+++ js/principal.js
@@ -10,4 +10,8 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+var tdIMC = paciente.querySelector(".info-imc");
+
 var imc = peso / (altura * altura);
+
+tdIMC.textContent = imc;
