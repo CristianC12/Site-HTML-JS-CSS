d155a2629a5b0b3f07427282e8dfc21783effb26
Cristian Martini <cristiamcarlosm@hotmail.com>
2021-07-06 18:32:55 -0300

    Adicionando validação de altura/peso

--- js/principal.js
+++ js/principal.js
@@ -12,6 +12,25 @@ var altura = tdAltura.textContent;
 
 var tdIMC = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
 
+var pesoValido = true;
+var alturaValida = true;
+
+    if(peso<=0 || peso >=1000){
+
+        console.log("Peso inválido");
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+    }
+
+    if (altura<=0 || altura>=3.00){
+
+        console.log("Altura inválida")
+        pesoValido = false;
+        tdIMC.textContent = "Altura inválida"
+    }
+
+if( alturaValida = true && pesoValido == true){
+var imc = peso / (altura * altura);
 tdIMC.textContent = imc;
+}
